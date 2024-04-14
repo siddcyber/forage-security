5f897201c71bd91bcb326cd23faeb013d4e900af
siddcyber <siddhantgulia@gmail.com>
2024-04-14 18:32:06 -0400

    Finished custom exercise(mean of amount grouped by type and fraud), fixed minor errors and made things prettier

--- task1.py
+++ task1.py
@@ -1,62 +1,43 @@
-# Test exercises here
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Read the dataset (`transactions.csv`) as a Pandas dataframe. Note that the first row of the CSV contains the column
-# names.
 def exercise_0(file):
     df = pd.read_csv(file)
     return df
 
 
-# Return the column names as a list from the dataframe.
 def exercise_1(df):
     return list(df.columns)
 
 
-# Return the first k rows from the dataframe.
 def exercise_2(df, k):
     return df.head(k)
 
 
-# Return a random sample of k rows from the dataframe.
 def exercise_3(df, k):
     return df.sample(k)
 
 
-# Return a list of the unique transaction types.
 def exercise_4(df):
     return list(df['type'].unique())
 
 
-# Return a Pandas series of the top 10 transaction destinations with frequencies.
 def exercise_5(df):
     return df['nameDest'].value_counts().head(10)
 
 
-# Return all the rows from the dataframe for which fraud was detected.
 def exercise_6(df):
     return df.loc[df['isFlaggedFraud'] == 1]
 
 
-# Bonus. Return a dataframe that contains the number of distinct destinations that each source has interacted with
-# to, sorted in descending order.
+
 def exercise_7(df):
     data = df.groupby('nameOrig')['nameDest'].nunique().sort_values(ascending=False)
     gk = pd.DataFrame(data).reset_index()
     return gk
 
 
-# Create graphs for the following.
-#
-# Transaction types bar chart, Transaction types split by fraud bar chart
-# Origin account balance delta v. Destination account balance delta scatter plot for Cash Out transactions
-# Ensure that the graphs have the following:
-# Title
-# Labeled Axes
-# The function plot the graph and then return a string containing a short description explaining the relevance of
-# the chart.
 def visual_1(df):
     def transaction_counts(df):
         return df['type'].value_counts()
@@ -83,7 +64,6 @@ def visual_1(df):
            'and how many were not (blue). '
 
 
-# Origin account balance delta v. Destination account balance delta scatter plot for Cash Out transactions
 def visual_2(df):
     def query(df):
         cashOut = df[df['type'] == 'CASH_OUT'].copy()
@@ -101,17 +81,15 @@ def visual_2(df):
            'from the origin account to the destination account.'
 
 
-#  aggregate of values grouped by types and split by fraud
+# This visualization allows us to compare the average transaction amounts across different transaction types and see
+# how fraud status influences these averages.
 def exercise_custom(df):
-    # df['isFraud'] = df['isFraud'].replace({0: 'Not Fraud', 1: 'Fraud'})
-    data = df.groupby(['type', 'isFraud'])['amount'].mean()
-    data = data.unstack(level='isFraud')
+    data = df.groupby(['type', 'isFraud'])['amount'].mean().unstack(level='isFraud')
     return data
 
 
-#  aggregate of values grouped by types and split by fraud bar graph
 def visual_custom(df):
-    fig = df.plot( kind='bar')
+    fig = exercise_custom(df).plot( kind='bar')
     fig.set_title('Mean Transaction Amount by Type and Fraud Status')
     fig.set_xlabel('transaction type')
     fig.set_ylabel('Mean transaction Amount')
@@ -119,13 +97,3 @@ def visual_custom(df):
     plt.xticks(rotation=45, ha='right')
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     return plt.show()
-    # return 'This visualization allows us to compare the average transaction amounts across different transaction types and see how fraud status influences these averages.'
-
-
-# Read the dataset (`transactions.csv`) as a Pandas dataframe. Note that the first row of the CSV contains the column
-# names.
-df = exercise_0('transactions.csv')
-print(exercise_1(df))
-print(exercise_4(df))
-print(exercise_custom(df))
-visual_custom(exercise_custom(df))
